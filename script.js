e80350da8c71ca1f72cc62aa056aa307f810b6f2
khushiweb434 <prabhakarkhushi8@gmail.com>
2026-05-23 16:25:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-function openMessage(){
+function showSurprise(){
 
-  document.getElementById("message")
-  .classList.remove("hidden");
+document.getElementById("message")
+.classList.remove("hidden");
 
-  document.getElementById("message")
-  .scrollIntoView({
-    behavior:'smooth'
-  });
+document.getElementById("message")
+.scrollIntoView({
+
+behavior:'smooth'
+
+});
 
 }
+
+const text =
+"I Love You More Than Words ❤️";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing")
+.innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,100);
+
+}
+
+}
+
+typing();
